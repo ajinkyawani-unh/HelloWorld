afc2fe4998e9731c16c8a73f139250f5c22cec80
ajinkyawani-unh <60481083+ajinkyawani-unh@users.noreply.github.com>
2020-01-30 19:53:53 -0500

    commit 1
    trial commit

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Ajinkya Wani on 1/23/20.
+//  Copyright © 2020 Ajinkya Wani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
